446227aaeef8117b79fbd4bca957d0d53bf0827e
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-25 14:47:36 +0700

    lagi mau edit column score di profileTable

--- controllers/profileDeedCont.js
+++ controllers/profileDeedCont.js
@@ -17,16 +17,19 @@ class ProfileDeed {
         DeedId: deedId
       })
       .then(success => {
+        console.log('ini looopins============================>')
+        // profileModel.setDataValues('score','score total')
         res.redirect(`/profile/${profileId}/userpage`)
       })
       .catch(err => console.log(err))
     })
+
   }
 
 
 
 
-  
+
 }
 
 module.exports = ProfileDeed
